eb5f68c8fa736b1902d93690b7c17635fe0c6a0f
Shirley Lin <r13922159@ntu.edu.tw>
2025-04-07 01:58:54 +0800

    fix evaluation too slowwwwww

--- student_agent.py
+++ student_agent.py
@@ -7,7 +7,9 @@ from gym import spaces
 import matplotlib.pyplot as plt
 import copy
 import random
-import math
+import gc
+
+
 
 
 from Game2048Env import Game2048Env
@@ -17,9 +19,16 @@ from n_tuple_design import get_patterns
 from TD_MCTS import TD_MCTS, TD_MCTS_Node
 
 patterns = get_patterns()
-approximator = NTupleApproximator(board_size=4, patterns=patterns)
-with open("train_file/td_learning/td_table_episode_1000.pkl", "rb") as f:
-    approximator.weights = pickle.load(f)
+# approximator = NTupleApproximator(board_size=4, patterns=patterns)
+
+def init_model():
+    global approximator
+    if approximator is None:
+        gc.collect() 
+        approximator = NTupleApproximator(board_size=4, patterns=patterns)
+        with open("train_file/td_learning/td_table_episode_1000.pkl", "rb") as f:
+            approximator.weights = pickle.load(f)
+
 
 
 def get_action(state, score):
@@ -28,6 +37,7 @@ def get_action(state, score):
     env.score = score
     
     root = TD_MCTS_Node(state, score)
+    init_model()
     td_mcts = TD_MCTS(env, approximator, iterations=50, exploration_constant=1.41, rollout_depth=10, gamma=0.99)
     
     
