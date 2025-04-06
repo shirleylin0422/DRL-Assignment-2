c7451500a29f53bcc2e3a4cd3295c9b7c1448060
Shirley Lin <r13922159@ntu.edu.tw>
2025-04-07 04:51:40 +0800

    use td approximator directly

--- student_agent.py
+++ student_agent.py
@@ -18,7 +18,7 @@ import pickle
 from n_tuple_design import get_patterns
 from TD_MCTS import TD_MCTS, TD_MCTS_Node
 
-patterns = get_patterns()
+
 approximator = None
 # approximator = NTupleApproximator(board_size=4, patterns=patterns)
 
@@ -26,33 +26,48 @@ def init_model():
     global approximator
     if approximator is None:
         gc.collect() 
-        approximator = NTupleApproximator(board_size=4, patterns=patterns)
-        with open("train_file/td_learning/td_table_episode_1000.pkl", "rb") as f:
+        approximator = NTupleApproximator(board_size=4, patterns=get_patterns())
+        with open("train_file/td_learning/td_table_episode_5000.pkl", "rb") as f:
             approximator.weights = pickle.load(f)
-
-
 
 def get_action(state, score):
     env = Game2048Env()
     env.board = state.copy() 
     env.score = score
     
-    root = TD_MCTS_Node(state, score)
-    init_model()
-    td_mcts = TD_MCTS(env, approximator, iterations=50, exploration_constant=1.41, rollout_depth=10, gamma=0.99)
-    
-    
-    # Run multiple simulations to build the MCTS tree
-    for _ in range(td_mcts.iterations):
-        td_mcts.run_simulation(root)
+    legal_moves = [a for a in range(4) if env.is_move_legal(a)]
 
-    # Select the best action (based on highest visit count)
-    best_act, _ = td_mcts.best_action_distribution(root)
-    
-    return best_act
+    best_value = -float('inf')
+    best_action = None
 
-    # return random.choice([0, 1, 2, 3]) # Choose a random action
-    
-    # You can submit this random agent to evaluate the performance of a purely random strategy.
+    for a in legal_moves:
+        env_copy = copy.deepcopy(env)
+        next_state, score_inc, done_flag, _ = env_copy.step(a)
+        v_next = approximator.value(next_state)
+        if v_next > best_value:
+            best_value = v_next
+            best_action = a
 
+    return best_action
+
+"""MCTS"""
+
+# def get_action(state, score):
+#     env = Game2048Env()
+#     env.board = state.copy() 
+#     env.score = score
+    
+#     root = TD_MCTS_Node(state, score)
+#     init_model()
+#     td_mcts = TD_MCTS(env, approximator, iterations=50, exploration_constant=1.41, rollout_depth=10, gamma=0.99)
+    
+    
+#     # Run multiple simulations to build the MCTS tree
+#     for _ in range(td_mcts.iterations):
+#         td_mcts.run_simulation(root)
+
+#     # Select the best action (based on highest visit count)
+#     best_act, _ = td_mcts.best_action_distribution(root)
+    
+#     return best_act
 
